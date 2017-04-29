b68f4dbf3d8ddf61bfc76ab2b12d52b97c04c3e7
andela-esitati <elsis.sitati@andela.com>
2017-04-29 19:44:55 +0300

    show disadvantage of for Each method

--- array-methods.js
+++ array-methods.js
@@ -31,13 +31,23 @@ const getEvenNumbers = function (number) {
 numbers.forEach(getEvenNumbers);
 console.log(evenNumbers);
 
+
 let numbers = [7, 2, 3, 8, 11, 1, 4, 15, 10];
+const doubleNumbers = function (list) {
+    // square each element of the array and store it in the result variable
+    let result = numbers.forEach(number => {
+        return number ** 2;
+    });
+    return result;
+}
+console.log(doubleNumbers(numbers)); // this will return undefined
 
-let evenNumbers = [];
-
-numbers.forEach(number => {
-    if (number % 2 === 0) {
-        evenNumbers.push(number);
-    } 
-});
-console.log(evenNumbers);
+let numbers = [7, 2, 3, 8, 11, 1, 4, 15, 10];
+const doubleNumbers = function (list) {
+    // square each element of the array and store it in the result variable
+    numbers.forEach(number => {
+        //print the result of the operation
+        console.log(number ** 2);
+    });
+}
+console.log(doubleNumbers(numbers)); // this will print the squared value of each element in the array
